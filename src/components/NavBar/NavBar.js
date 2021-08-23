fe10b3724c1bf75e4257b631acdd35116499a23b
momchilmitev <momchilmitev87@gmail.com>
2021-08-23 20:22:53 +0200

    adding nav bar component and the markup

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <img src="" alt="mobile navigation button" />
+      <a href="/"><img src="" alt="logo" /></a>
+      <section>
+        <a href="/about">About</a>
+        <a href="/location">Location</a>
+        <a href="/careers">Careers</a>
+        <button>Get Scootin</button>
+      </section>
+    </nav>
+  );
+}
+
+export default NavBar;
