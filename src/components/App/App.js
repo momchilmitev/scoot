d3557dbfa433563e347f0f9b1e142f0489317556
momchilmitev <momchilmitev87@gmail.com>
2021-08-29 19:05:38 +0200

    adding react router and the 4 pages components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,29 @@
 import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { About } from '../pages/About';
+import { Careers } from '../pages/Careers';
+import { Locations } from '../pages/Locations';
 
 function App() {
-	return <h1>Hi Momchi</h1>;
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/abote">
+					<About />
+				</Route>
+				<Route path="/careers">
+					<Careers />
+				</Route>
+				<Route path="/locations">
+					<Locations />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/pages/Locations.js
@@ -0,0 +1,3 @@
+export const Locations = () => {
+	return <h1>Locations</h1>;
+};
