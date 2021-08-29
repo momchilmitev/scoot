f4abd93f5d691955e05ae1cd780460d47b8bb369
momchilmitev <momchilmitev87@gmail.com>
2021-08-29 10:33:37 +0200

    adding the navigation with the rest of the pages

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	entry: {
 		index: './src/assets/js/index.js',
+		index: './src/assets/js/about.js',
+		index: './src/assets/js/careers.js',
+		index: './src/assets/js/locations.js',
 	},
 	devServer: {
 		port: 8080,
@@ -14,6 +17,27 @@ module.exports = {
 			inject: true,
 			chunks: ['index'],
 			filename: 'index.html',
+		}),
+		new HtmlWebpackPlugin({
+			title: 'About Page',
+			template: './src/pages/about.html',
+			inject: true,
+			chunks: ['index'],
+			filename: 'about.html',
+		}),
+		new HtmlWebpackPlugin({
+			title: 'Careers Page',
+			template: './src/pages/careers.html',
+			inject: true,
+			chunks: ['index'],
+			filename: 'careers.html',
+		}),
+		new HtmlWebpackPlugin({
+			title: 'Locations Page',
+			template: './src/pages/locations.html',
+			inject: true,
+			chunks: ['index'],
+			filename: 'locations.html',
 		}),
 	],
 };
