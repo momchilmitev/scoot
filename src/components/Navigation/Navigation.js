d5aa238e731d2e22aab4c74b86d14f863559f331
momchilmitev <momchilmitev87@gmail.com>
2021-08-30 17:06:26 +0200

    adding the icons for mobile menu in the navigation component and implementing the logic for switching them

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,19 @@
-import logo from '../../assets/images/logo.svg';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.svg';
+import hamburgerIcon from '../../assets/icons/hamburger.svg';
+import closeIcon from '../../assets/icons/close.svg';
 
 export const Navigation = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<nav className="navigation">
+			<img
+				src={isOpen ? closeIcon : hamburgerIcon}
+				alt="navigation triger button"
+				onClick={() => setIsOpen(!isOpen)}
+			/>
 			<Link to="/">
 				<img className="navigation__logo" src={logo} alt="logo" />
 			</Link>
